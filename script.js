0315515f50d872654822148bfbc3a642c7d22cb9
trinigutierrezm <tgutierrezmosquera@itba.edu.ar>
2022-11-21 10:38:45 -0300

    hice el js con los id

---
+++ script.js
@@ -0,0 +1,57 @@
+// Variables
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Collar Surfer',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 2,
+        nombre: 'Collar Simona',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 3,
+        nombre: 'Collar Rainbow',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 4,
+        nombre: 'Collar Soft Pink',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 5,
+        nombre: 'Collar Liana',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 6,
+        nombre: 'Collar Perlas',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 7,
+        nombre: 'Collar Lilo',
+        precio: 650,
+        imagen: ''
+    },
+    {
+        id: 8,
+        nombre: 'Aritos Soft Pink',
+        precio: 300,
+        imagen: ''
+    },
+    {
+        id: 9,
+        nombre: 'Aritos Green ',
+        precio: 300,
+        imagen: ''
+    },
+    ];
