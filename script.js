1756759e14f1dcd625f83438443e5be925be2b69
trinigutierrezm <tgutierrezmosquera@itba.edu.ar>
2022-11-22 16:33:20 -0300

    jj

--- script.js
+++ script.js
@@ -77,3 +77,18 @@ const containerCartProducts = document.querySelector('.container-cart-products')
 btnCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle('hidden-cart')
 })
+
+let arrayProducto =[]
+let Total = 0
+function sumaProductos(Valoritem, Producto) {
+    arrayProducto.push(Producto)
+  Total = Total + Valoritem
+  document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + Total; 
+  console.log(arrayProducto);
+  document.getElementById("valorProducto").value = Total
+  document.getElementById("listaProductos").value=arrayProducto
+  return Total}
+
+function Limpiar() {
+  Total=0
+  document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + 0; }
